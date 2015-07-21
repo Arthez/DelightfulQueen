88578ced8ea97e8e39f58aff3c8c7805b068852e
Artur Ratowski <arturatow@gmail.com>
2015-07-21 17:11:56 +0200

    Bootcamp day 7 project.

--- app/src/main/java/com/droidsonroids/delightfulqueen/MainActivity.java
+++ app/src/main/java/com/droidsonroids/delightfulqueen/MainActivity.java
@@ -3,38 +3,33 @@ package com.droidsonroids.delightfulqueen;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.droidsonroids.awesomeprogressbar.AwesomeProgressBar;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends Activity {
 
     @Bind(R.id.super_awesome_progress_bar)
     AwesomeProgressBar mSuperAwesomeProgressBar;
-//    @Bind(R.id.activity_main_btn_alpha)
-//    Button mBtnAlpha;
-//    @Bind(R.id.activity_main_btn_rotate)
-//    Button mBtnRotate;
-//    @Bind(R.id.activity_main_btn_scale)
-//    Button mBtnScale;
-//    @Bind(R.id.activity_main_btn_translate)
-//    Button mBtnTranslate;
-//    @Bind(R.id.activity_main_btn_move)
-//    Button mBtnMove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_custom_view);
         ButterKnife.bind(this);
-        mSuperAwesomeProgressBar.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-                mSuperAwesomeProgressBar.play();
-            }
-        });
+    }
+
+    @OnClick(R.id.success_button)
+    public void playSuccessAnimation() {
+        mSuperAwesomeProgressBar.play(true);
+    }
+
+    @OnClick(R.id.failure_button)
+    public void playFailureAnimation() {
+        mSuperAwesomeProgressBar.play(false);
     }
 
     @Override
--- awesomeprogressbar/src/main/java/com/droidsonroids/awesomeprogressbar/AwesomeProgressBar.java
+++ awesomeprogressbar/src/main/java/com/droidsonroids/awesomeprogressbar/AwesomeProgressBar.java
@@ -1,32 +1,45 @@
 package com.droidsonroids.awesomeprogressbar;
 
 import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
+import android.view.animation.OvershootInterpolator;
 
 public class AwesomeProgressBar extends View {
-    private static final float DEFAULT_LINE_THICKNESS = 12.0f;
+    private static final float DEFAULT_STROKE_THICKNESS = 5.0f;
     private static final int DEFAULT_ANIMATION_DURATION = 800;
+    private static final float DEFAULT_CIRCLE_RADIUS = 100f;
+    private static final float DEFAULT_CROSS_SCALE = 0.9f;
 
     private Paint mBackgroundPaint;
     private Paint mProgressBarPaint;
     private ValueAnimator mProgressAnimation;
+    private AnimatorSet mAnimatorSet;
 
+    private boolean mSuccessState;
     private float mProgressValue;
+    private float mSuccessValue;
     private int mAnimationDuration;
     private int mBackgroundColor;
     private int mProgressBarColor;
 
-    private float mLineThickness;
+    private RectF mRectF;
+    private float mCenterX;
+    private float mCenterY;
+    private float mCircleRadius;
+    private float mStrokeThickness;
     private boolean isAnimationInitialized = false;
 
-    private enum State {RUNNING_STATE, END_STATE, IDLE_STATE}
+    private enum State {RUNNING_STATE, IDLE_STATE, SUCCESS_STATE}
     private State mState;
 
     public AwesomeProgressBar(Context context) {
@@ -39,23 +52,34 @@ public class AwesomeProgressBar extends View {
     }
 
     private void initialize(Context context, AttributeSet attrs) {
-        if(attrs != null) {
+        if (attrs != null) {
             TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.AwesomeProgressBar);
             try {
-                mLineThickness = array.getDimension(R.styleable.AwesomeProgressBar_lineThickness, DEFAULT_LINE_THICKNESS);
-                mAnimationDuration = array.getInteger(
-                    R.styleable.AwesomeProgressBar_animationDuration, DEFAULT_ANIMATION_DURATION);
+                mStrokeThickness = array.getDimension(R.styleable.AwesomeProgressBar_strokeThickness,
+                        DEFAULT_STROKE_THICKNESS);
+                mCircleRadius = array.getDimension(R.styleable.AwesomeProgressBar_circleRadius,
+                        DEFAULT_CIRCLE_RADIUS);
+                mAnimationDuration = array.getInteger(R.styleable.AwesomeProgressBar_animationDuration,
+                        DEFAULT_ANIMATION_DURATION);
                 mBackgroundColor = array.getColor(R.styleable.AwesomeProgressBar_backgroundColor,
-                    getDefaultBackgroundColor());
-                mProgressBarColor = array.getColor(R.styleable.AwesomeProgressBar_progressBarColor, getDefaultProgressBarColor());
+                        getDefaultBackgroundColor());
+                mProgressBarColor = array.getColor(R.styleable.AwesomeProgressBar_progressBarColor,
+                        getDefaultProgressBarColor());
             } finally {
                 array.recycle();
             }
         }
         mBackgroundPaint = new Paint();
         mBackgroundPaint.setColor(mBackgroundColor);
+        mBackgroundPaint.setStyle(Paint.Style.STROKE);
+        mBackgroundPaint.setStrokeWidth(mStrokeThickness);
+        mBackgroundPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+
         mProgressBarPaint = new Paint();
         mProgressBarPaint.setColor(mProgressBarColor);
+        mProgressBarPaint.setStyle(Paint.Style.STROKE);
+        mProgressBarPaint.setStrokeWidth(mStrokeThickness);
+        mProgressBarPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
         mState = State.IDLE_STATE;
     }
 
@@ -70,40 +94,87 @@ public class AwesomeProgressBar extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        if(!isAnimationInitialized) {
-            setupAnimations(w, h);
+        mCenterX = w / 2;
+        mCenterY = h / 2;
+        mRectF = new RectF(mCenterX - mCircleRadius, mCenterY - mCircleRadius,
+                mCenterX + mCircleRadius, mCenterY + mCircleRadius);
+
+        if (!isAnimationInitialized) {
+            setupAnimations();
             isAnimationInitialized = true;
         }
     }
 
-    private void setupAnimations(final int w, final int h) {
-        mProgressAnimation = ValueAnimator.ofFloat(0, w);
+    private void setupAnimations() {
+        mProgressAnimation = ValueAnimator.ofFloat(0, 360);
         mProgressAnimation.setDuration(mAnimationDuration);
         mProgressAnimation.setInterpolator(new LinearInterpolator());
-        mProgressAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override public void onAnimationUpdate(ValueAnimator animation) {
+        mProgressAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() { //
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
                 mProgressValue = (float) animation.getAnimatedValue();
                 invalidate();
             }
         });
+
         mProgressAnimation.addListener(new Animator.AnimatorListener() {
-            @Override public void onAnimationStart(Animator animation) {
+            @Override
+            public void onAnimationStart(Animator animation) {
                 mState = State.RUNNING_STATE;
             }
 
-            @Override public void onAnimationEnd(Animator animation) {
-                postDelayed(new Runnable() {
-                    @Override public void run() {
-                        mState = State.END_STATE;
-                    }
-                }, 500);
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mAnimatorSet.start();
             }
 
-            @Override public void onAnimationCancel(Animator animation) {
+            @Override
+            public void onAnimationCancel(Animator animation) {
 
             }
 
-            @Override public void onAnimationRepeat(Animator animation) {
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+        ValueAnimator successAnimationOvershoot;
+        successAnimationOvershoot = ValueAnimator.ofFloat(0, mCircleRadius / DEFAULT_CROSS_SCALE);
+        successAnimationOvershoot.setDuration(mAnimationDuration);
+        successAnimationOvershoot.setInterpolator(new OvershootInterpolator());
+        successAnimationOvershoot.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mSuccessValue = (float) animation.getAnimatedValue();
+                invalidate();
+            }
+        });
+
+        ObjectAnimator rotateAnimation = ObjectAnimator.ofFloat(this, "rotation", 0f, 360f);
+        rotateAnimation.setDuration(mAnimationDuration / 2);
+        rotateAnimation.setInterpolator(new LinearInterpolator());
+
+        mAnimatorSet = new AnimatorSet();
+        mAnimatorSet.playTogether(rotateAnimation, successAnimationOvershoot);
+        mAnimatorSet.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                mState = State.SUCCESS_STATE;
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
 
             }
         });
@@ -112,15 +183,42 @@ public class AwesomeProgressBar extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawRect(0, getHeight() / 2 - mLineThickness / 2, getWidth(),
-            getHeight() / 2 + mLineThickness / 2, mBackgroundPaint);
-        if(mState == State.RUNNING_STATE) {
-            canvas.drawRect(0, getHeight() / 2 - mLineThickness / 2, mProgressValue,
-                getHeight() / 2 + mLineThickness / 2, mProgressBarPaint);
+        drawProgressCircle(canvas, mBackgroundPaint);
+
+        if (mState == State.RUNNING_STATE) {
+            canvas.drawArc(mRectF, 270, mProgressValue, false, mProgressBarPaint);
+        } else if (mState == State.SUCCESS_STATE && mSuccessState) {
+            drawSuccessElement(canvas);
+        } else if (mState == State.SUCCESS_STATE && !mSuccessState) {
+            drawFailureElement(canvas);
         }
     }
 
-    public void play() {
+    public void play(boolean successFlag) {
+        mSuccessState = successFlag;
+        if (mProgressAnimation.isRunning()) {
+            mState = State.IDLE_STATE;
+        }
         mProgressAnimation.start();
+    }
+
+    private void drawProgressCircle(Canvas canvas, Paint circleColor) {
+        canvas.drawCircle(mCenterX, mCenterY, mCircleRadius, circleColor);
+    }
+
+    private void drawSuccessElement(Canvas canvas) {
+        drawProgressCircle(canvas, mProgressBarPaint);
+        canvas.drawLine(mCenterX, mCenterY, mCenterX + mSuccessValue / 2, mCenterY, mProgressBarPaint);
+        canvas.drawLine(mCenterX, mCenterY, mCenterX - mSuccessValue / 2, mCenterY, mProgressBarPaint);
+        canvas.drawLine(mCenterX, mCenterY, mCenterX, mCenterY + mSuccessValue / 2, mProgressBarPaint);
+        canvas.drawLine(mCenterX, mCenterY, mCenterX, mCenterY - mSuccessValue / 2, mProgressBarPaint);
+    }
+
+    private void drawFailureElement(Canvas canvas) {
+        drawProgressCircle(canvas, mProgressBarPaint);
+        canvas.drawLine(mCenterX, mCenterY, mCenterX + mSuccessValue / 2, mCenterY - mSuccessValue / 2, mProgressBarPaint);
+        canvas.drawLine(mCenterX, mCenterY, mCenterX - mSuccessValue / 2, mCenterY + mSuccessValue / 2, mProgressBarPaint);
+        canvas.drawLine(mCenterX, mCenterY, mCenterX + mSuccessValue / 2, mCenterY + mSuccessValue / 2, mProgressBarPaint);
+        canvas.drawLine(mCenterX, mCenterY, mCenterX - mSuccessValue / 2, mCenterY - mSuccessValue / 2, mProgressBarPaint);
     }
 }
